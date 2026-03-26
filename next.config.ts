2fb6a32b2330186916d8deba825a7e71e516f1f2
William-LP <10053686+William-LP@users.noreply.github.com>
2026-03-26 16:39:20 +0100

    image unoptimized + output:export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     basePath: "/mini-coffre.fr",
-    assetPrefix: "/mini-coffre.fr"
+    assetPrefix: "/mini-coffre.fr",
+    output: "export",
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
